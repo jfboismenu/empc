9b1620d190c2efc3cbd90e5821dc4a8334c9f97d
Jean-Francois Boismenu <jfboismenu@gmail.com>
2020-07-12 18:55:58 -0400

    bios-like test`:w

--- tests/test_mov.asm
+++ tests/test_mov.asm
@@ -1,8 +1,30 @@
-    global start
-    section .text
+; MIT License
+;
+; Copyright (c) 2020 Jean-François Boismenu
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
 
-# Test moving data from a register to memory and back
-# register, value, address
+ORG 0xffff0000    ; 64k BIOS
+BITS 16
+
+; Test moving data from a register to memory and back
+; register, value, address
 %macro  reg2mem2reg 3
     mov %1, %2
     mov [%3], %1
@@ -10,8 +32,8 @@
     mov %1, [%3]
 %endmacro
 
-# Test moving data from register to another
-# register register value
+; Test moving data from register to another
+; register register value
 %macro reg2reg 3
     mov %1, %3
     mov %2, %1
@@ -19,35 +41,39 @@
     mov %1, %2
 %endmacro
 
-start:
-# Our first opcode, yes!
-    nop
-
+test:
     reg2mem2reg ax, 0x1234, 0x9000
     reg2mem2reg bx, 0x2345, 0xA000
     reg2mem2reg cx, 0x3456, 0xB000
     reg2mem2reg dx, 0x4567, 0xC000
 
-    reg2mem2reg al, 0x10, 0x9100
-    reg2mem2reg bl, 0x20, 0xA100
-    reg2mem2reg cl, 0x30, 0xB100
-    reg2mem2reg dl, 0x40, 0xC100
+    reg2mem2reg al, 0x10, 0x91
+    reg2mem2reg bl, 0x20, 0xA1
+    reg2mem2reg cl, 0x30, 0xB1
+    reg2mem2reg dl, 0x40, 0xC1
 
-    reg2mem2reg ah, 0x10, 0x9100
-    reg2mem2reg bh, 0x20, 0xA100
-    reg2mem2reg ch, 0x30, 0xB100
-    reg2mem2reg dh, 0x40, 0xC100
+    reg2mem2reg ah, 0x10, 0x92
+    reg2mem2reg bh, 0x20, 0xA2
+    reg2mem2reg ch, 0x30, 0xB2
+    reg2mem2reg dh, 0x40, 0xC2
 
-    reg2reg ax, bx, 0x12
-    reg2reg ax, cx, 0x34
-    reg2reg ax, dx, 0x56
+    reg2reg ax, bx, 0x1234
+    reg2reg ax, cx, 0x3456
+    reg2reg ax, dx, 0x5678
 
-    reg2reg bx, cx, 0x78
-    reg2reg bx, dx, 0x9A
+    reg2reg bx, cx, 0x789A
+    reg2reg bx, dx, 0x9ABC
 
-    reg2reg cx, dx, 0xBC
+    reg2reg cx, dx, 0xBCDE
 
     mov ax, ax
     mov bx, bx
     mov cx, cx
     mov dx, dx
+
+    hlt
+
+; PC jumps to FFFF0 on startup, so let's jump to the beginning of the test.
+TIMES   0xfff0-($-$$) DB 0x90
+    jmp test
+TIMES   0x10000-($-$$) DB 0x90
