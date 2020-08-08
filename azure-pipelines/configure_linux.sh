d0a216b36b444a43ca86ea23815dd29585b24219
Jean-Francois Boismenu <jfboismenu@gmail.com>
2020-08-08 09:57:21 -0400

    upgrading stdlibc++

--- azure-pipelines/configure_linux.sh
+++ azure-pipelines/configure_linux.sh
@@ -27,4 +27,5 @@ set -e
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install g++-10
+sudo apt-get upgrade libstdc++
 sudo apt-get install nasm ninja-build cmake
